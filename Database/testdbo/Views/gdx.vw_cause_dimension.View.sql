bf721e6f22f0a864a21fa74341b6a5a6086ed4b3
jeromecrdevprd <jerome.cruz@gatesfoundation.org>
2021-07-26 14:16:29 -0700

    add

--- Database/testdbo/Views/gdx.vw_cause_dimension.View.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE VIEW gdx.vw_cause_dimension
-AS
-SELECT 
-ihme_cause_id,
-[ihme_gbd_2016_cause_nm] AS ihme_cause_nm
-FROM staging.cause_dimension
--- Database/testdbo/Views/gdx.vw_country_dimension.View.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE VIEW [gdx].[vw_country_dimension] AS
-SELECT 
-iso_country_cd,
-iso_country_nm,
-country_nm AS ipm_country_nm,
-ihme_location_cd
-FROM
-[staging].[country_dimension]
-GO
